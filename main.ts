a18b4175170fb99165f9606ea4797b69b688b4cb
codewithurooj <fatimaurooj152@gmail.com>
2024-04-20 10:59:44 -0700

    Adding To Do List App

---
+++ main.ts
@@ -0,0 +1,28 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+
+let todoList = [ ];
+let conditions ="True";
+console.log("\n\t Wellcome to code with Syeda Urooj-Todo List Application\n");
+while (conditions ){
+    let  addTask =await inquirer.prompt([
+        {
+            name:"task",
+            type:"input",
+            message:"Enter your new task:",
+        }
+    ]);
+    todoList.push(addTask.task);
+    console.log(`${addTask.task} Task added to Todo List Successfully`);
+    let addMoreTask =await inquirer.prompt([
+        {
+            name:"addmore",
+            type:"confirm",
+            message:"DO You Want to Add More Task?",
+            default: "false"
+        }
+    ]);
+    conditions = addMoreTask.addmore;
+    console.log("Your updated Todo-List",todoList);
+
+}
